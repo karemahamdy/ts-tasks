9444d663dc7d008392d46d763118000587e0450f
Karema <karemahamdy51@gmail.com>
2025-04-17 12:51:54 +0200

    add first types code

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-var url = 'https://jsonplaceholder.typicode.com/todos/1';
+const axios_1 = __importDefault(require("axios"));
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+let id, title, completed;
 axios_1.default.get(url)
-    .then(function (response) {
+    .then(response => {
     console.log(response.data);
+    id = response.data.id;
+    title = response.data.title;
+    completed = response.data.completed;
+    console.log(id, title, completed);
 })
-    .catch(function (error) {
+    .catch(error => {
     console.error("Error fetching data:", error);
 });
--- index.ts
+++ index.ts
@@ -2,10 +2,25 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url)
-  .then(response => {
+interface ToDO  {
+  id: number,
+  title: string,
+  completed: boolean 
+}
+let id: number, title: string, completed: boolean;
+
+axios.get<ToDO>(url)
+  .then(response => {  
     console.log(response.data);
+    id = response.data.id
+    title = response.data.title
+   completed = response.data.completed  
+console.log(
+  id, title, completed
+)
   })
   .catch(error => {
     console.error("Error fetching data:", error);
   });
+
+
