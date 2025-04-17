4e3e2a17f2b6edcaca48ab44875aa17d27ed45f3
Karema <karemahamdy51@gmail.com>
2025-04-17 12:22:47 +0200

    feat: add first compile of placeholder api

---
+++ index.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
