a53d0fa68c87c8f3d18adff3b31f60b492d07f85
Karema <karemahamdy51@gmail.com>
2025-04-18 21:22:17 +0200

    feat: add function

--- var.ts
+++ var.ts
@@ -27,3 +27,12 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+const sum: (a: number, b: number) => number = (a, b) => {
+  return a + b
+}
+// or 
+const sum1 = (a: number, b: number) : number  => {
+  return a + b
+}
+
